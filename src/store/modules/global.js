6575e740f401ce65949926e49624e258f53ae6f3
Dr. Sergey Pogodin <dr.pogodin@gmail.com>
2017-01-05 00:31:54 +0100

    Issue #19 - When you log in (successfully) the login window remains open (also when you log out)
    Fixes #19

--- src/store/modules/global.js
+++ src/store/modules/global.js
@@ -10,12 +10,26 @@ const userApi = new UserApi(config.api.basePath);
 //------------------------------------------------------------------------------
 // Constants
 
+const LOGIN_ACTION_FAILURE = 'LOGIN_ACTION_FAILURE';
+const LOGIN_ACTION_SUCCESS = 'LOGIN_ACTION_SUCCESS';
+
+const LOGIN_REDIRECT = {
+  admin: '/admin',
+  consumer: '/browse-provider',
+  pilot: '/pilot',
+  provider: '/dashboard',
+};
+
 const LOGOUT_ACTION = 'LOGOUT_ACTION';
 const USER_INFO_KEY = 'userInfo';
 
 // ------------------------------------
 // Actions
 // ------------------------------------
+
+// TODO: Any use of these local variables should be eliminated!
+// Their current usage should be entirely replaced using the redux state,
+// and action payloads!
 let isLogged = false;
 let hasError = false;
 let errorText = '';
@@ -29,29 +43,6 @@ function loadUserInfo() {
   }
   return userInfo;
 }
-
-export const sendLoginRequest = (values) => new Promise((resolve) => {
-  userApi.login(values.email, values.password).then((authResult) => {
-    isLogged = true;
-    hasError = false;
-    userInfo = authResult;
-    localStorage.setItem(USER_INFO_KEY, JSON.stringify(authResult));
-    if (authResult.user.role === 'consumer') {
-      browserHistory.push('/browse-provider');
-    } else if (authResult.user.role === 'provider') {
-      browserHistory.push('/dashboard');
-    } else if (authResult.user.role === 'admin') {
-      browserHistory.push('/admin');
-    } else if (authResult.user.role === 'pilot') {
-      browserHistory.push('/pilot');
-    }
-  }).catch((err) => {
-    isLogged = false;
-    hasError = true;
-    errorText = JSON.parse(err.responseText);
-  });
-  resolve();
-});
 
 export const sendSignupRequest = (values) => new Promise((resolve) => {
   userApi.register(values.firstName, values.lastName, values.email, values.password).then(() => {
@@ -68,14 +59,25 @@ export const sendSignupRequest = (values) => new Promise((resolve) => {
 
 export const toggleNotification = createAction('TOGGLE_NOTIFICATION');
 
-export const loginAction = createAction('LOGIN_ACTION');
+export const loginAction = (data) => (dispatch) => {
+  userApi.login(data.email, data.password).then((res) => {
+    localStorage.setItem(USER_INFO_KEY, JSON.stringify(res));
+    dispatch({type: LOGIN_ACTION_SUCCESS});
+    browserHistory.push(LOGIN_REDIRECT[res.user.role]);
+  }).catch((failure) => {
+    dispatch({
+      type: LOGIN_ACTION_FAILURE,
+      payload: JSON.parse(failure.response).error,
+    });
+  });
+};
 
-export const logoutAction = () => dispatch => {
+export const logoutAction = () => (dispatch) => {
   browserHistory.push('/home');
   dispatch({
-    type: LOGOUT_ACTION
+    type: LOGOUT_ACTION,
   });
-}
+};
 
 export const signupAction = createAction('SIGNUP_ACTION');
 
@@ -90,8 +92,19 @@ export default handleActions({
   [toggleNotification]: (state, action) => ({
     ...state, toggleNotif: action.payload,
   }),
-  [loginAction]: (state) => ({
-    ...state, loggedUser: isLogged, hasError, errorText, user: (loadUserInfo() ? loadUserInfo().user : {}),
+  [LOGIN_ACTION_FAILURE]: (state, action) => ({
+    ...state,
+    loggedUser: false,
+    hasError: true,
+    errorText: action.payload,
+    user: {},
+  }),
+  [LOGIN_ACTION_SUCCESS]: (state) => ({
+    ...state,
+    loggedUser: true,
+    hasError: false,
+    errorText: '',
+    user: (loadUserInfo() ? loadUserInfo().user : {}),
   }),
   [LOGOUT_ACTION]: (state) => {
     localStorage.removeItem(USER_INFO_KEY);
